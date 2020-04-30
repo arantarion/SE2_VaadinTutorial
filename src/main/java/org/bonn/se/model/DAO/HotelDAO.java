cc556867b0f6174305eecf231c2bb26b06b66a98
hweck <henry.weckermann@smail.inf.h-brs.de>
2020-04-30 14:25:26 +0200

    Implemented HotelDAO

--- src/main/java/org/bonn/se/model/DAO/HotelDAO.java
+++ src/main/java/org/bonn/se/model/DAO/HotelDAO.java
@@ -1,4 +1,72 @@
 package org.bonn.se.model.DAO;
 
+import org.bonn.se.model.objects.dto.Hotel;
+import org.bonn.se.model.objects.dto.Role;
+import org.bonn.se.model.objects.dto.User;
+import org.bonn.se.process.control.exceptions.DatabaseException;
+import org.bonn.se.services.db.JDBCConnection;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class HotelDAO {
+
+    public static HotelDAO dao = null;
+
+    private HotelDAO() {
+    }
+
+    public static HotelDAO getInstance() {
+        if (dao == null) {
+            dao = new HotelDAO();
+        }
+        return dao;
+    }
+
+    public List<Hotel> getHotelbyLocation(String ort) {
+
+        Statement statement = null;
+
+        try {
+            statement = JDBCConnection.getInstance().getStatement();
+        } catch (DatabaseException e) {
+            return null;
+        }
+
+        ResultSet rs = null;
+
+        try {
+            rs = statement.executeQuery("SELECT * FROM realm.hotel WHERE realm.hotel.ort = \'" + ort + "\' ");
+        } catch (SQLException ex) {
+            Logger.getLogger(JDBCConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        if (rs == null) return null;
+
+        List<Hotel> liste = new ArrayList<>();
+        Hotel hotel = null;
+
+        try {
+            while (rs.next()) {
+                hotel = new Hotel();
+                hotel.setId(rs.getInt(1));
+                hotel.setName(rs.getString(2));
+                hotel.setOrt(rs.getString(3));
+                hotel.setDesc(rs.getString(4));
+                liste.add(hotel);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(JDBCConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return liste;
+
+    }
+
+
 }
